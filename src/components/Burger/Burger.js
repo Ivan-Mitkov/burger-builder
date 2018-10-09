fb00a36887d7594142477b7e360cef65d7006fc4
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-09 15:36:51 +0300

    adding dynamic ingredients coninues

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,23 +5,31 @@ import BurgerIngredient from './BurgerIngredien/BurgerIngredient';
 
 const burger = (props) => {
     //sazdavame masiv s keys i dalgina kolkoto sa ingredients
-    let transformedIngr=Object.keys(props.ingredients)
+    let transformedIngr = Object.keys(props.ingredients)
         //v gornia masiv sazdava masivi s tolkova ingred kolkoto ima v burgera
-        .map((ingKey)=>{return [...Array(props.ingredients[ingKey])]
-            //vav vseki edin ot nested masivite vkava jsx s index imeto i indeksa i type imeto
-                                   .map((_,index)=>{
-                                          return(
-                                                <BurgerIngredient key={ingKey+index} type={ingKey}/>
-                                          )
-                                        })
-                            });
-    console.log(transformedIngr);
+        .map((ingKey) => {
+            return [...Array(props.ingredients[ingKey])]
+                //vav vseki edin ot nested masivite vkava jsx s index imeto i indeksa i type imeto
+                .map((_, index) => {
+                    return (
+                        <BurgerIngredient key={ingKey + index} type={ingKey} />
+                    )
+                })
+        })
+        //flatten the nested array
+        .reduce((arr,el)=>{
+            return arr.concat(el);
+        },[]);
+        if(transformedIngr.length===0){
+            transformedIngr=<p>Please start adding ingredients </p>
+        }
+        console.log(transformedIngr)
     return (
         <div className={BurgerCss.Burger}>
-            <BurgerIngredient type='bread-top'/>
+            <BurgerIngredient type='bread-top' />
             {transformedIngr}
-            <BurgerIngredient type='bread-bottom'/>
-            
+            <BurgerIngredient type='bread-bottom' />
+
         </div>
     );
 }
