98797a82c9fd79f263e741d47adc26bf45347b07
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-09 15:13:56 +0300

    first atep to dynamic ingredients-add array of ingredients and method for transforming state in BurgerBuilder to array in BurgerIngredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,24 @@ import BurgerCss from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredien/BurgerIngredient';
 
 const burger = (props) => {
+    //sazdavame masiv s keys i dalgina kolkoto sa ingredients
+    let transformedIngr=Object.keys(props.ingredients)
+        //v gornia masiv sazdava masivi s tolkova ingred kolkoto ima v burgera
+        .map((ingKey)=>{return [...Array(props.ingredients[ingKey])]
+            //vav vseki edin ot nested masivite vkava jsx s index imeto i indeksa i type imeto
+                                   .map((_,index)=>{
+                                          return(
+                                                <BurgerIngredient key={ingKey+index} type={ingKey}/>
+                                          )
+                                        })
+                            });
+    console.log(transformedIngr);
     return (
         <div className={BurgerCss.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+            {transformedIngr}
             <BurgerIngredient type='bread-bottom'/>
+            
         </div>
     );
 }
