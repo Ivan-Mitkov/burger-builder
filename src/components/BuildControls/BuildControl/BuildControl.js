7da7fa1bc782aada0af418926198176f5018814a
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-09 17:05:24 +0300

    add and remove ingredients finished, showing total price

--- src/components/BuildControls/BuildControl/BuildControl.js
+++ src/components/BuildControls/BuildControl/BuildControl.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import BC from './BuildControl.module.css';
 
-const buildControl=(props)=>{
-return (
-    <div className={BC.BuildControl}>
-        <div className={BC.label}>{props.label}</div>
-        <button className={BC.Less}>Less</button>
-        <button className={BC.More}>More</button>
-    </div>
-)
+const buildControl = (props) => {
+    return (
+        <div className={BC.BuildControl}>
+            <div className={BC.label}>{props.label}</div>
+            <button className={BC.Less}
+                onClick={props.less}
+                disabled={props.disabled}>Less</button>
+            <button className={BC.More}
+                //receive prop for more from BuildControls
+                onClick={props.more}>More</button>
+        </div>
+    )
 }
 
 export default buildControl;
--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -3,21 +3,28 @@ import React from 'react';
 import BCs from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl'
 
-const controls=[
-    {label:'Salad', type:'salad'},
-    {label:'Bacon', type:'bacon'},
-    {label:'Meat', type:'meat'},
-    {label:'Cheese', type:'cheese'}
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Cheese', type: 'cheese' }
 
 ]
-const buildControls=(props)=>{
-return (
-    <div className={BCs.BuildControls}>
-        {controls.map((bc)=>{
-            return <BuildControl key={bc.label} label={bc.label}/>
-        })}
-    </div>
-)
+const buildControls = (props) => {
+    return (
+        <div className={BCs.BuildControls}>
+        <p>Current Price: <strong> {props.price.toFixed(2)}</strong></p>
+            {controls.map((bc) => {
+                return <BuildControl
+                    key={bc.label}
+                    label={bc.label}
+                    //add prop for more, connect it to type and then should be passed to BuildControl
+                    more={() => props.ingredientAdded(bc.type)}
+                    less={() => props.ingredientRemoved(bc.type)} 
+                    disabled={props.disabled[bc.type]}/>
+            })}
+        </div>
+    )
 }
 
 export default buildControls;
