68a2d2df11195dedbbb6f83092444f1c0552fb61
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-21 14:14:23 +0300

    redirect after purchase using redux

--- src/containers/checkout/Checkout.js
+++ src/containers/checkout/Checkout.js
@@ -4,8 +4,14 @@ import { connect } from "react-redux";
 
 import CheckoutSumary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
+import * as actions from "../../store/actions/index";
 
 class Checkout extends Component {
+  //for redirect after order
+  componentWillMount() {
+    this.props.onInitPurchase();
+  }
+
   //In App Route is used in two container BurgerBuilder and this one So here we also has access to history
   checkoutCancelHandler = () => {
     this.props.history.goBack();
@@ -19,9 +25,15 @@ class Checkout extends Component {
     //initialy when taking ings from DB there null, so there is error while trying to load
     //in order to solve this problem
     let summary = <Redirect to="/" />;
+
     if (this.props.ings) {
+      //when purchased is done
+      const purchasedRedirect = this.props.purchased ? (
+        <Redirect to="/" />
+      ) : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSumary
             ingredients={this.props.ings}
             checkoutCancel={this.checkoutCancelHandler}
@@ -40,8 +52,19 @@ class Checkout extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
-    price: state.burgerBuilder.totalPrice
+    price: state.burgerBuilder.totalPrice,
+    purchased: state.order.purchased
   };
 };
 
-export default connect(mapStateToProps)(Checkout);
+//redicting
+const mapDispatchToProps = dispatch => {
+  return {
+    onInitPurchase: () => dispatch(actions.purchaseInit())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Checkout);
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -2,10 +2,16 @@ import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
   orders: [],
-  loading: false
+  loading: false,
+  purchased: false
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.PURCHASE_INIT:
+      return {
+        ...state,
+        purchased: false
+      };
     case actionTypes.PURCHASE_BURGER_START:
       return {
         ...state,
@@ -20,6 +26,7 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
+        purchased: true,
         orders: [...state.orders, newOrder]
       };
     case actionTypes.PURCHASE_BURGER_FAILED:
@@ -27,6 +34,7 @@ const reducer = (state = initialState, action) => {
         ...state,
         loading: true
       };
+
     default:
       return state;
   }
