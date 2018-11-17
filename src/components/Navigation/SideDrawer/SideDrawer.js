5bb3fbd42a61ea680ca83faa671a724edcc792a2
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-11-17 18:03:08 +0200

    add lazy loading for some routes and some polish

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,33 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../../Logo/Logo';
-import NavigationItems from './../NavigationItems/NavigationItems';
-import classes from './SideDrawer.module.css';
-import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Hoc';
+import Logo from "../../Logo/Logo";
+import NavigationItems from "./../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Hoc";
 
-const sideDrawer = (props) => {
-    let cssOpenCloseClasses=[classes.SideDrawer,classes.Close];
-    if(props.open){
-        cssOpenCloseClasses=[classes.SideDrawer,classes.Open];
-    }
-    const cssClasses= cssOpenCloseClasses.join(' ');
-    return (
-        <Aux>
-            <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={cssClasses}>
-                <div className={classes.Logo}>
-                    <Logo></Logo>
-                </div>
+const sideDrawer = props => {
+  let cssOpenCloseClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    cssOpenCloseClasses = [classes.SideDrawer, classes.Open];
+  }
+  const cssClasses = cssOpenCloseClasses.join(" ");
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={cssClasses} onClick={props.closed}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
 
-                <nav>
-                    <NavigationItems isAuthenticated={props.isAuth}></NavigationItems>
-                </nav>
-            </div>
-
-        </Aux>
-
-    )
-}
+        <nav>
+          <NavigationItems isAuthenticated={props.isAuth} />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
 
 export default sideDrawer;
